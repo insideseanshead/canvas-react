abdb70ba98b4e3a71b827a027f90b96364f3a697
Sean Morgan <insideseanshead@gmail.com>
2021-12-12 15:33:37 -0800

    added drawing functionality to canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,16 +1,50 @@
-import { useRef, useEffect } from 'react'
-import context from 'react-bootstrap/esm/AccordionContext'
+import { useRef, useEffect } from "react";
+import context from "react-bootstrap/esm/AccordionContext";
 
 const Canvas = (props) => {
-    const canvasRef = useRef(null)
-    
-    useEffect(() => {
-        const canvas = canvasRef.current
-        const ctx = canvas.getContext('2d')
-    }, [])
-    return (
-        <canvas ref={canvasRef} {...props} id='canvas' width='800' height='600' />
-    )
-}
+  const canvasRef = useRef(null);
+  // Drawing functions
+  const draw = (ctx) => {
+    const canvas = document.getElementById('canvas')
+    let drawing = false;
 
-export default Canvas
+    const startDrawing = (e) => {
+      drawing = true;
+      drawLine(e);
+    };
+
+    const endDrawing = (e) => {
+      drawing = false;
+      ctx.beginPath()
+    };
+
+    const drawLine = (e) => {
+        if(!drawing) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY)
+    };
+
+    // Event Listeners
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mouseup', endDrawing);
+    canvas.addEventListener('mousemove', drawLine);
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    draw(context);
+  }, [draw]);
+
+  return (
+    <canvas ref={canvasRef} {...props} id="canvas" width="800" height="600" />
+  );
+};
+
+export default Canvas;
