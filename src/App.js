6c62753d495f797d11ca287858af498cfe68a822
Sean Morgan <insideseanshead@gmail.com>
2021-12-12 15:10:20 -0800

    used useEffect and useRef to set up canvas logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Canvas from './components/Canvas'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Controls from './components/Controls';
+import {useState, useEffect, useRef } from 'react'
+import Canvas from "./components/Canvas";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Controls from "./components/Controls";
 
 function App() {
+  // State for controls
+  
+  // Set up Canvas
+
+  // Drawing Functions
+
+  // Click Events
+
+  // Slider Controls
+
+  // Button functions
   return (
     <div className="App">
       <Canvas />
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,6 +1,15 @@
-const Canvas = () => {
+import { useRef, useEffect } from 'react'
+import context from 'react-bootstrap/esm/AccordionContext'
+
+const Canvas = (props) => {
+    const canvasRef = useRef(null)
+    
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+    }, [])
     return (
-        <canvas id='canvas' width='800' height='600' />
+        <canvas ref={canvasRef} {...props} id='canvas' width='800' height='600' />
     )
 }
 
